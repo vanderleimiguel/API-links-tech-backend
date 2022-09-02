780231ebfe6b8caa515468a97c1353903e6253fe
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-09-02 11:26:47 -0300

    create swagger

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ const express = require('express')
 const cors = require('cors')
 const connectDataBase = require('./databases/mongo/connection/database')
 const linkRoute = require('./routes/link.route')
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./docs/swagger.json')
 
 const port = process.env.PORT || 3000
 const app = express()
 
 connectDataBase()
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(express.json())
 app.use(cors())
