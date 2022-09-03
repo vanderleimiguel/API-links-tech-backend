1c0002caab36357d56ec7678d8f47eac39a05a5e
vanderlei miguel <vanderleimiguel@hotmail.com>
2022-09-03 16:05:49 -0300

    update id

--- src/databases/mongo/schema/link.js
+++ src/databases/mongo/schema/link.js
@@ -1,7 +1,6 @@
 const { Schema, model } = require('mongoose')
 
 const LinkSchema = new Schema({
-  id: { type: String, required: true },
   title: { type: String, required: true },
   url: { type: String, required: true }
 })
